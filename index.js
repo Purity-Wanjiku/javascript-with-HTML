1f60bbb4fbcc15225b94db991a4e708da63ece5e
Purityndirangu <ndiranguwanjikupurity@gmail.com>
2023-05-25 20:04:15 +0300

    added new items to the list

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 document.getElementsByClassName('body');
-document.getElementById('body').style.backgroundColor = 'green';
+document.getElementById('body').style.backgroundColor = 'silver';
 
 
 document.getElementById('title').style.color = 'green';
+document.getElementById('title2').style.textTransform = 'uppercase'
+
+
+let fruitList = document.getElementById('fruList');
+let newFruit = document.createElement('li');
+newFruit.textContent = 'Avocado';
+fruitList.appendChild(newFruit);
+
+
+let veggieList = document.getElementById('vegList');
+let newVeggie = document.createElement('li');
+newVeggie.textContent = 'Broccoli';
+veggieList.appendChild(newVeggie)
